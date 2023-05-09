a6f9235b3ed4dbe4164adec7c508bb991aeb6967
asepsaepulanwar218 <asepsaepulanwar9a@gmail.com>
2023-05-09 21:18:26 +0700

    penambahan flsher

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -20,6 +20,11 @@ class Mahasiswa extends Controller{
 
     public function tambah() {
         if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0 ) {
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,12 @@
 <div class="container mt-5">
 
     <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+        </div>
+    </div>
+
+    <div class="row">
         <div class="col-6">
 
         <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#formMahasiswa">
